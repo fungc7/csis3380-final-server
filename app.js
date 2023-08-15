7a54ba9ba1eb1225dd4cf66d876f1557331122a6
Ivan Fung <fungc7@student.douglascollege.ca>
2023-08-15 14:52:42 -0700

    default

--- app.js
+++ app.js
@@ -5,56 +5,60 @@ const { Schema, model } = mongoose;
 const app = express();
 app.use(express.json());
 
-const uri = "mongodb+srv://tempuser:123@cluster0.f9d6o.gcp.mongodb.net/Exam";
+// const uri = "mongodb+srv://tempuser:123@cluster0.f9d6o.gcp.mongodb.net/Exam";
 
-const data = [
-    { name : "Ivan Fung",
-        sid : "300371938"
-    }
-];
+// const data = [
+//     { name : "Ivan Fung",
+//         sid : "300371938"
+//     }
+// ];
 
-// Create and/or connect to the database
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+// // Create and/or connect to the database
+// mongoose.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
 
-console.log('Connected');
+// console.log('Connected');
 
-const quizSchema = new Schema({
-    name: String,
-    sid: String,
-});
+// const quizSchema = new Schema({
+//     name: String,
+//     sid: String,
+// });
 
-// Use the "bookInfo" collection
-const Stud = model("QuizSchema", quizSchema, "quizes");
+// // Use the "bookInfo" collection
+// const Stud = model("QuizSchema", quizSchema, "quizes");
 
-app.get("/", async (req, res, next) => {
-    try {
-        const records = [];
-        data.forEach((record) => {
-            records.push(new Stud(record));
-        })
-        await Stud.insertMany(records);
-        res.json({
-            message: "Successful insert.",
-            dbserver: uri,
-        })
-        res.status(204).end();
-    } catch(err){
-        next(err);
-    }
-})
+// app.get("/", async (req, res, next) => {
+//     try {
+//         const records = [];
+//         data.forEach((record) => {
+//             records.push(new Stud(record));
+//         })
+//         await Stud.insertMany(records);
+//         res.json({
+//             message: "Successful insert.",
+//             dbserver: uri,
+//         })
+//         res.status(204).end();
+//     } catch(err){
+//         next(err);
+//     }
+// })
 
-app.get("/test", async (req, res, next) => {
-    try {
-        const records = await Stud.find({});
-        res.json(records);
-        res.status(204).end();
-    }
-    catch(err) {
-        next(err);
-    }
+// app.get("/test", async (req, res, next) => {
+//     try {
+//         const records = await Stud.find({});
+//         res.json(records);
+//         res.status(204).end();
+//     }
+//     catch(err) {
+//         next(err);
+//     }
+// })
+
+app.get("/", (req, res, next) => {
+    res.json("Hello World.");
 })
 
 app.use((req, res, next) => {
